ebbd815ab8ae6e84f410e13dbe0ecced5fa31911
Mikhael <misha2007sur@gmail.com>
2024-04-21 03:15:34 +0500

    add a registration; link a ccs file to temp

--- src/forms/registrarion_form.py
+++ src/forms/registrarion_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField, EmailField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class RegistrationForm(FlaskForm):
     nickname = StringField("nickname", validators=[DataRequired()])
     email = EmailField("email", validators=[DataRequired()])
-    password = StringField("password", validators=[DataRequired()])
-    password_again = StringField("password_again", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    password_again = PasswordField("password_again", validators=[DataRequired()])
     submit = SubmitField("Register")
